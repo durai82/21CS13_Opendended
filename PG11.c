1f8f2cef260078d7ecf13e59bacc9d2e24ca1ca6
durai82 <111646131+durai82@users.noreply.github.com>
2022-08-20 20:10:05 +0530

    Update PG11.c

--- PG11.c
+++ PG11.c
@@ -1,3 +1,16 @@
 //reverse a number program
 //input:123
 //output:321
+#include<stdio.h>  
+ int main()    
+{    
+int n=123, reverse=0, rem;       
+  while(n!=0)    
+  {    
+     rem=n%10;    
+     reverse=reverse*10+rem;    
+     n/=10;    
+  }    
+  printf("%d",reverse);    
+return 0;  
+}   
